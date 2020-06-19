1c0d4b6947a6a3f2b95bc6f00c7ba5e7a57637de
Andrew <slvmdev66@gmail.com>
2020-06-19 18:21:35 +0300

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -21,7 +21,7 @@ const getCity = async (city) => {
 };
 
 const getIP = async () => {
-  const base = "http://ip-api.com/json/";
+  const base = "https://ip-api.com/json/";
   const res = await fetch(base);
   const data = await res.json();
   return data;
